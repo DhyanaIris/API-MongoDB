8683a904e60c6fdc12b6f7cb25a9eeb9f0080b2d
DhyanaIris <dhyanairis@gmail.com>
2023-11-09 22:18:34 -0300

    update arrumado

--- src/api/controller/pessoaController.js
+++ src/api/controller/pessoaController.js
@@ -1,5 +1,6 @@
 const Pessoa = require("../models/pessoaSchema");
 const faker = require('faker');
+const mongoose = require('mongoose');
 
 const pessoaController = {
   // Cria um novo item Pessoa
@@ -24,7 +25,7 @@ const pessoaController = {
   createManyRandom: async (req, res) => {
     try {
       // Alterar o valor para inserção de mais ou menos itens no banco
-      const quantidade = 10000;
+      const quantidade = 100000;
       const randomPessoas = [];
 
       for (let i = 0; i < quantidade; i++) {
@@ -93,41 +94,52 @@ const pessoaController = {
     }
   },
 
+  // Função assíncrona para atualizar aleatoriamente dados em muitos documentos no MongoDB
   updateManyRandom: async (req, res) => {
     try {
-      // Alterar o valor para atualizar mais ou menos itens no banco
-      const quantidade = 10000;
-      // Consulta para encontrar documentos a serem atualizados
-      const query = { _id: { $exists: true } }; // Isso irá corresponder a qualquer documento com um _id existente
-      const result = await Pessoa.find(query).exec();
-      if (result && result.length > 0) {
-        const updatePromises = [];
-        for (let i = 0; i < quantidade; i++) {
-          const randomIndex = Math.floor(Math.random() * result.length);
-          const randomPerson = result[randomIndex];
-          // Gere valores aleatórios usando o Faker para os campos que deseja atualizar
-          const update = {
-            $set: {
-              nome: faker.name.findName(),
-              dataNascimento: faker.date.past(),
-              profissao: faker.name.jobTitle(),
-              cor: faker.random.arrayElement(['Branco', 'Negro', 'Pardo', 'Amarelo', 'Indígena']),
-              estado: faker.address.state(),
-              cidade: faker.address.city(),
-            },
-          };
-          // Crie uma promessa para a atualização e adicione-a ao array
-          updatePromises.push(Pessoa.updateOne({ _id: randomPerson._id }, update).exec());
+      // Consulta todos os documentos no banco de dados MongoDB
+      const documentos = await Pessoa.find({});
+
+      // Define a quantidade desejada de documentos a serem atualizados
+      const quantidade = 100000;
+
+      // Gera uma matriz de índices disponíveis para seleção aleatória
+      const availableIndices = Array.from({ length: documentos.length }, (_, i) => i);
+
+      // Loop para atualizar documentos até atingir a quantidade desejada ou não haver mais índices disponíveis
+      for (let i = 0; i < quantidade && availableIndices.length > 0; i++) {
+        // Seleciona aleatoriamente um índice disponível
+        const randomIndex = Math.floor(Math.random() * availableIndices.length);
+        const selectedIndex = availableIndices.splice(randomIndex, 1)[0];
+
+        // Obtém o documento correspondente ao índice selecionado
+        const doc = documentos[selectedIndex];
+
+        try {
+          // Atualiza os campos do documento com dados fictícios usando a biblioteca Faker
+          doc.nome = faker.name.findName();
+          doc.dataNascimento = faker.date.past();
+          doc.profissao = faker.name.jobTitle();
+          doc.cor = faker.random.arrayElement(['Branco', 'Negro', 'Pardo', 'Amarelo', 'Indígena']);
+          doc.estado = faker.address.state();
+          doc.cidade = faker.address.city();
+
+          // Salva as alterações no MongoDB usando o Mongoose
+          await doc.save();
+
+          // Loga uma mensagem indicando que o documento foi atualizado com sucesso
+          console.log(`Documento com ID ${doc._id} atualizado.`);
+        } catch (error) {
+          // Loga um erro se houver algum problema ao atualizar o documento
+          console.log(`Erro ao atualizar o documento com ID ${doc._id}:`, error);
         }
-        // Aguarde a conclusão de todas as atualizações
-        await Promise.all(updatePromises);
-        res.json({ message: `${quantidade} pessoas atualizadas no MongoDB` });
-      } else {
-        res.json({ message: 'Nenhum documento encontrado para atualizar' });
       }
-    } catch (error) {
-      console.error('Erro ao atualizar pessoas no MongoDB:', error);
-      res.status(500).json({ error: 'Erro ao atualizar pessoas no MongoDB' });
+      // Responde com sucesso após a conclusão da atualização de documentos
+      res.status(200).send('Documentos atualizados com sucesso.');
+    } catch (err) {
+      // Loga um erro se houver algum problema ao buscar os documentos no MongoDB
+      console.log('Erro ao buscar documentos:', err);
+      res.status(500).send('Erro ao buscar documentos.');
     }
   },
 
@@ -149,17 +161,17 @@ const pessoaController = {
     try {
       // Consulta para encontrar todos os documentos a serem excluídos
       const query = {};
-  
+
       // Execute a exclusão em massa
       const result = await Pessoa.deleteMany(query);
-  
+
       res.json({ message: `${result.deletedCount} pessoas excluídas do MongoDB` });
     } catch (error) {
       console.error('Erro ao excluir dados do MongoDB:', error);
       res.status(500).json({ error: 'Erro ao excluir dados do MongoDB' });
     }
   },
-  
+
 }
 
 module.exports = pessoaController;
